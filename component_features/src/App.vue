dc1553f7aae95da976241d57ef0424f0d47d5e74
Георгий Бухаров <gbu@crtweb.ru>
2018-03-27 16:03:31 +0500

    learned: slots and dynamic components

---
+++ component_features/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <button @click="selectedComponent = 'appQuote'">Quote</button>
+        <button @click="selectedComponent = 'appAuthor'">Author</button>
+        <button @click="selectedComponent = 'appNew'">New</button>
+        <hr>
+        <component :is="selectedComponent">
+          <p>Default content</p>
+        </component>
+        <!-- <app-quote>
+          <h2 slot="title">{{ quoteTitle }}</h2>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus perspiciatis voluptates magni repellat eum qui dolorem provident mollitia rerum consectetur error numquam deleniti architecto natus quibusdam, a ea excepturi inventore.</p>
+        </app-quote> -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Quote from "./components/Quote.vue";
+import Author from "./components/Author.vue";
+import New from "./components/New.vue";
+export default {
+  data() {
+    return {
+      quoteTitle: 'The Quote',
+      selectedComponent: 'appQuote',
+    }
+  },
+  components: {
+    appQuote: Quote,
+    appAuthor: Author,
+    appNew: New,
+  }
+};
+</script>
+
+<style>
+
+</style>
